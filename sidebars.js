a0387462d1edbd20710338495399a70a34c2f660
Tomas R <rtomas@gmail.com>
2024-12-05 23:42:56 -0300

    fix link errors

--- sidebars.js
+++ sidebars.js
@@ -246,7 +246,7 @@ module.exports = {
                 {
                   type: 'doc',
                   label: 'SIWX',
-                  id: 'appkit/features/siwx/index'
+                  id: 'appkit/features/siwx/default'
                 }
               ]
             },
