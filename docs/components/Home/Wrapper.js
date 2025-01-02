787c56481e529738ae5746330fe3b58687f65cb6
Rohit Ramesh <rrohitramesh710@gmail.com>
2025-01-02 12:59:24 +0530

    homepage redesign

--- docs/components/Home/Wrapper.js
+++ docs/components/Home/Wrapper.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import SmallContainer from './SmallContainer'
 import LargeContainer from './LargeContainer'
+import CenteredContainer from './CenteredContainer'
+import SupportContainer from './SupportContainer'
 
 const Wrapper = ({ items, type, fit = true, ...props }) => {
   return (
@@ -9,32 +11,56 @@ const Wrapper = ({ items, type, fit = true, ...props }) => {
       className="home__wrapper"
       style={{
         paddingTop: !fit ? '1rem' : 'inherit',
-        ...props.style
+        ...props.style,
       }}
     >
-      {type === 'small'
-        ? items.map((item, index) => (
-            <SmallContainer
-              key={index}
-              href={item.href}
-              type={item.type}
-              name={item.name}
-              icon={item.icon}
-              isWhite={item.isWhite || false}
-            />
-          ))
-        : items.map((item, index) => (
-            <LargeContainer
-              key={index}
-              href={item.href}
-              type={item.type}
-              name={item.name}
-              icon={item.icon}
-              fit={fit}
-              isWhite={item.isWhite || false}
-              description={item.description}
-            />
-          ))}
+      {type === 'small' &&
+        items.map((item, index) => (
+          <SmallContainer
+            key={index}
+            href={item.href}
+            type={item.type}
+            name={item.name}
+            icon={item.icon}
+            isWhite={item.isWhite || false}
+          />
+        ))}
+
+      {type === 'large' &&
+        items.map((item, index) => (
+          <LargeContainer
+            key={index}
+            href={item.href}
+            type={item.type}
+            name={item.name}
+            icon={item.icon}
+            fit={fit}
+            isWhite={item.isWhite || false}
+            description={item.description}
+          />
+        ))}
+
+      {type === 'centered' &&
+        items.map((item, index) => (
+          <CenteredContainer
+            key={index}
+            href={item.href}
+            name={item.name}
+            icon={item.icon}
+            description={item.description}
+          />
+        ))}
+
+      {type === 'support' &&
+        items.map((item, index) => (
+          <SupportContainer
+            key={index}
+            href={item.href}
+            name={item.name}
+            icon={item.icon}
+            description={item.description}
+          />
+        ))}
     </div>
   )
 }
