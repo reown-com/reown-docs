c3ecf1be7ab9bde94ed833ed2932483ac664812b
Tomas R <rtomas@gmail.com>
2025-01-17 14:53:07 -0300

    add to sidebar

--- sidebars.js
+++ sidebars.js
@@ -291,7 +291,8 @@ module.exports = {
                 { type: 'doc', label: 'EVM Transactions using Ethers', id: 'appkit/recipes/ethers-send-transaction'},
                 { type: 'doc', label: 'Solana Transactions using AppKit', id: 'appkit/recipes/solana-send-transaction'},
                 { type: 'doc', label: 'Bitcoin Transactions using AppKit', id: 'appkit/recipes/bitcoin-send-transaction'},
-                { type: 'doc', label: 'Support Send Calls', id: 'appkit/recipes/switching-to-send-calls' } 
+                { type: 'doc', label: 'Support Send Calls', id: 'appkit/recipes/switching-to-send-calls' },
+                { type: 'doc', label: 'Sponsoring Transaction with our Paymaster', id: 'appkit/recipes/sponsoring-first-transaction' } 
               ]
             },
             {
