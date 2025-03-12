de8940cb546a94afbb691a75bd6feeceb82e8da2
Rohit Ramesh <rrohitramesh710@gmail.com>
2025-03-12 12:22:01 +0100

    updating search bar

--- src/theme/Navbar/Search/index.js
+++ src/theme/Navbar/Search/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import clsx from 'clsx'
-import styles from './styles.module.css'
-export default function NavbarSearch({ children, className }) {
-  return <div className={clsx(className, styles.searchBox)}>{children}</div>
+import React from "react";
+import { DocSearch } from "@docsearch/react";
+
+export default function SearchBar() {
+  return (
+    <DocSearch
+      appId="FNT2FF5Z1N"
+      apiKey="858103ff345e1d20a487ee99ea8fa03a"
+      indexName="reown"
+      transformItems={(items) =>
+        items.map((item) => ({
+          ...item,
+          title: `${item.hierarchy.lvl1} (${item.framework || "General"})`, // Append framework
+        }))
+      }
+    />
+  );
 }
